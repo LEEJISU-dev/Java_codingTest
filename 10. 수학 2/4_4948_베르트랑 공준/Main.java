3d1ffe9ef4f1ceeee60851167d44bfcb3bc65755
LEEJISU <amilly0819@gmail.com>
2020-09-28 22:50:11 +0900

    Add 10_4_4948

--- 10. 수학 2/4_4948_베르트랑 공준/Main.java
+++ 10. 수학 2/4_4948_베르트랑 공준/Main.java
@@ -2,22 +2,47 @@ import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) {
-		int a = 0; // 고정비용
-		int b = 0; // 가변비용
-		int c = 0; // 가격
-		
-		Scanner sc = new Scanner(System.in);
-		a = sc.nextInt();
-		b = sc.nextInt();
-		c = sc.nextInt();
+	public static boolean prime[];
 
-		if (b >= c) {
-			System.out.println(-1);
-		} else {
-			System.out.println(a/(c-b)+1);
-		}
+	public static void main(String[] args) {
+		int n = 0; 		// 자연수 n
+		int count = 0; // 소수 카운트
+		Scanner sc = new Scanner(System.in);
 		
+
+		while (true) {
+			n = sc.nextInt();
+			if (n == 0) {
+				break;
+			}
+			prime = new boolean[2 * n +1]; // 배열 생성
+			get_prime();
+
+			for (int i = n+1; i <= 2 * n; i++) {
+				if (prime[i] == false) { // 소수이면 카운트
+					count++;
+				}
+			}
+			System.out.println(count);
+			count = 0;
+		}
+	}
+
+	/*
+	 * 에라토스테네스의 체 
+	 * " k=2 부터 √N 이하까지 반복하여 자연수들 중 k를 제외한 k의 배수들을 제외시킨다"
+	 */
+	public static void get_prime() {
+		prime[0] = true;
+		prime[1] = true;
+
+		// i=2부터 √N 이하까지 반복
+		for (int i = 2; i <= Math.sqrt(prime.length); i++) {
+			// i의 배수들을 모두 제외
+			for (int j = i * i; j < prime.length; j += i) {
+				prime[j] = true;
+			}
+		}
 	}
 
 }
