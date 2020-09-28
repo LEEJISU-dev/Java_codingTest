ca0cb3994a2298c4ee110bd9056e3165c1be69a2
LEEJISU <amilly0819@gmail.com>
2020-09-28 22:26:13 +0900

    Add 10. 수학2

--- 10. 수학 2/1_1978_소수찾기/Main.java
+++ 10. 수학 2/1_1978_소수찾기/Main.java
@@ -3,21 +3,31 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		int a = 0; // 고정비용
-		int b = 0; // 가변비용
-		int c = 0; // 가격
-		
-		Scanner sc = new Scanner(System.in);
-		a = sc.nextInt();
-		b = sc.nextInt();
-		c = sc.nextInt();
+		int input = 0; // 입력받은 수의 개수
+		int n = 0; // 입력받은 수(소수인지 판별)
+		int tmp = 0;	// 나누어 떨어지는 수 카운트
+		int count = 0; // 소수 카운트
 
-		if (b >= c) {
-			System.out.println(-1);
-		} else {
-			System.out.println(a/(c-b)+1);
+		Scanner sc = new Scanner(System.in);
+		input = sc.nextInt();
+
+		for (int i = 0; i < input; i++) {
+			n = sc.nextInt();
+			for (int j = 2; j <= n; j++) {
+				if (n % j == 0) {
+					tmp++;
+				}
+			}
+			if (tmp < 2) {
+				count++;
+				if (n == 1) {
+					count--;
+				}
+			}
+			tmp = 0;
 		}
-		
+		System.out.println(count);
+
 	}
 
 }
--- 10. 수학 2/2_2581_소수/Main.java
+++ 10. 수학 2/2_2581_소수/Main.java
@@ -2,22 +2,51 @@ import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) {
-		int a = 0; // 고정비용
-		int b = 0; // 가변비용
-		int c = 0; // 가격
-		
-		Scanner sc = new Scanner(System.in);
-		a = sc.nextInt();
-		b = sc.nextInt();
-		c = sc.nextInt();
+	public static boolean prime[];
 
-		if (b >= c) {
+	public static void main(String[] args) {
+		int m = 0; // 자연수 m
+		int n = 0; // 자연수 n
+		int sum = 0; // 소수의 합
+		int min = Integer.MAX_VALUE; // 소수의 최솟값
+		Scanner sc = new Scanner(System.in);
+		m = sc.nextInt();
+		n = sc.nextInt();
+
+		prime = new boolean[n + 1]; // 배열 생성
+		get_prime();
+
+		for (int i = m; i <= n; i++) {
+			if (prime[i] == false) { // 소수이면
+				sum += i;
+				if (min == Integer.MAX_VALUE) {
+					min = i;
+				}
+			}
+		}
+		// 소수가 없을 경우
+		if (sum == 0) {
 			System.out.println(-1);
 		} else {
-			System.out.println(a/(c-b)+1);
+			System.out.println(sum);
+			System.out.println(min);
 		}
-		
+
+	}
+
+	/*
+	 * 에라토스테네스의 체 
+		" k=2 부터 √N 이하까지 반복하여 자연수들 중 k를 제외한 k의 배수들을 제외시킨다"
+	 */
+	public static void get_prime() {
+		prime[0] = true;
+		prime[1] = true;
+
+		for (int i = 2; i <= Math.sqrt(prime.length); i++) {
+			for (int j = i * i; j < prime.length; j+=i) {
+				prime[j] = true;
+			}
+		}
 	}
 
 }
