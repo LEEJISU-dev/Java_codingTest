7edec64ee248be5f74241a17da37a4f1be9d4b89
LEEJISU <amilly0819@gmail.com>
2020-09-28 22:32:06 +0900

    Add 10_3_1929

--- 10. 수학 2/3_1929_소수구하기/Main.java
+++ 10. 수학 2/3_1929_소수구하기/Main.java
@@ -2,22 +2,42 @@ import java.util.Scanner;
 
 public class Main {
 
+	public static boolean prime[];
+
 	public static void main(String[] args) {
-		int a = 0; // 고정비용
-		int b = 0; // 가변비용
-		int c = 0; // 가격
+		int m = 0; // 자연수 m
+		int n = 0; // 자연수 n
 		
 		Scanner sc = new Scanner(System.in);
-		a = sc.nextInt();
-		b = sc.nextInt();
-		c = sc.nextInt();
+		m = sc.nextInt();
+		n = sc.nextInt();
 
-		if (b >= c) {
-			System.out.println(-1);
-		} else {
-			System.out.println(a/(c-b)+1);
+		prime = new boolean[n + 1]; // 배열 생성
+		get_prime();
+
+		for (int i = m; i <= n; i++) {
+			if (prime[i] == false) { // 소수이면
+				System.out.println(i);
+			}
 		}
+
+	}
+
+	/*
+	 * 에라토스테네스의 체 
+	 * " k=2 부터 √N 이하까지 반복하여 자연수들 중 k를 제외한 k의 배수들을 제외시킨다"
+	 */
+	public static void get_prime() {
+		prime[0] = true;
+		prime[1] = true;
 		
+		// i=2부터  √N 이하까지 반복
+		for (int i = 2; i <= Math.sqrt(prime.length); i++) {
+			// i의 배수들을 모두 제외
+			for (int j = i * i; j < prime.length; j+=i) {
+				prime[j] = true;
+			}
+		}
 	}
 
 }
