c8c827e8b22728afbe5d200210537534c9e7abbb
LEEJISU <amilly0819@gmail.com>
2020-10-14 21:55:27 +0900

    Add 11-1

--- 11. 재귀/1_10872_팩토리얼/Main.java
+++ 11. 재귀/1_10872_팩토리얼/Main.java
@@ -1,33 +1,22 @@
 import java.util.Scanner;
 
 public class Main {
-
 	public static void main(String[] args) {
-		int input = 0; // 입력받은 수의 개수
-		int n = 0; // 입력받은 수(소수인지 판별)
-		int tmp = 0;	// 나누어 떨어지는 수 카운트
-		int count = 0; // 소수 카운트
-
+		int n = 0; // 입력받은 정수
+		int sum = 0; // 결과값
 		Scanner sc = new Scanner(System.in);
-		input = sc.nextInt();
+		n = sc.nextInt();
+		sum = get_Factorial(n);
+		System.out.println(sum);
 
-		for (int i = 0; i < input; i++) {
-			n = sc.nextInt();
-			for (int j = 2; j <= n; j++) {
-				if (n % j == 0) {
-					tmp++;
-				}
-			}
-			if (tmp < 2) {
-				count++;
-				if (n == 1) {
-					count--;
-				}
-			}
-			tmp = 0;
+	}
+
+	// 팩토리얼을 구하는 재귀함수
+	public static int get_Factorial(int n) {
+		if (n <= 1) {
+			return 1;
 		}
-		System.out.println(count);
-
+		return n * get_Factorial(n - 1);
 	}
 
 }
