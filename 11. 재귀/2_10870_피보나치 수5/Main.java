256972c91e8af483351fc70e7b1c6842c00e95ea
LEEJISU <amilly0819@gmail.com>
2020-10-14 22:14:06 +0900

    Add 11-2

--- 11. 재귀/2_10870_피보나치 수5/Main.java
+++ 11. 재귀/2_10870_피보나치 수5/Main.java
@@ -1,33 +1,25 @@
 import java.util.Scanner;
 
 public class Main {
-
 	public static void main(String[] args) {
-		int input = 0; // 입력받은 수의 개수
-		int n = 0; // 입력받은 수(소수인지 판별)
-		int tmp = 0;	// 나누어 떨어지는 수 카운트
-		int count = 0; // 소수 카운트
+		int n = 0; // 입력받은 정수
 
 		Scanner sc = new Scanner(System.in);
-		input = sc.nextInt();
+		n = sc.nextInt();
 
-		for (int i = 0; i < input; i++) {
-			n = sc.nextInt();
-			for (int j = 2; j <= n; j++) {
-				if (n % j == 0) {
-					tmp++;
-				}
-			}
-			if (tmp < 2) {
-				count++;
-				if (n == 1) {
-					count--;
-				}
-			}
-			tmp = 0;
+		System.out.println(fibo_num(n));
+
+	}
+
+	// 피보나치을 구하는 재귀함수
+	public static int fibo_num(int n) {
+		if (n == 0) {
+			return 0;
 		}
-		System.out.println(count);
-
+		if (n == 1) {
+			return 1;
+		}
+		return fibo_num(n - 1) + fibo_num(n - 2);
 	}
 
 }
