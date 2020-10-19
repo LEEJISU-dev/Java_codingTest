3700d09c7d1f138c249ebe20ebd03d28d055cd07
LEEJISU <amilly0819@gmail.com>
2020-10-19 21:23:03 +0900

    Add 11-4

--- 11. 재귀/4_11729_하노이 탑 이동 순서/Main.java
+++ 11. 재귀/4_11729_하노이 탑 이동 순서/Main.java
@@ -1,33 +1,30 @@
 import java.util.Scanner;
 
 public class Main {
-
+	public static StringBuilder sb = new StringBuilder();
+	
 	public static void main(String[] args) {
-		int input = 0; // 입력받은 수의 개수
-		int n = 0; // 입력받은 수(소수인지 판별)
-		int tmp = 0;	// 나누어 떨어지는 수 카운트
-		int count = 0; // 소수 카운트
+		int n = 0; // 원판의 개수
 
 		Scanner sc = new Scanner(System.in);
-		input = sc.nextInt();
-
-		for (int i = 0; i < input; i++) {
-			n = sc.nextInt();
-			for (int j = 2; j <= n; j++) {
-				if (n % j == 0) {
-					tmp++;
-				}
-			}
-			if (tmp < 2) {
-				count++;
-				if (n == 1) {
-					count--;
-				}
-			}
-			tmp = 0;
-		}
-		System.out.println(count);
-
+		n = sc.nextInt();
+		sb.append((int)(Math.pow(2, n)-1)).append('\n');
+		
+		Hanoi(n,1,2,3);
+		System.out.println(sb);
+		
 	}
-
+	public static void Hanoi(int n, int start, int mid, int to) {
+		// 이동할 원반의 수가 1개일 경우
+		if(n==1) {
+			sb.append(start+" "+to+"\n");
+			return;
+		}
+		// 1. n-1개를 A에서 B로 이동
+		Hanoi(n-1, start, to, mid);
+		// 2. 1개를 A에서 C로 이동
+		sb.append(start+ " "+to+"\n");
+		// 3. n-1개를 B에서 C로 이동
+		Hanoi(n-1, mid, start, to);
+	}
 }
