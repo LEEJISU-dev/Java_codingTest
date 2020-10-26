f6789c72549f6b03642c5ef475c6550d7f820d6e
LEEJISU <amilly0819@gmail.com>
2020-10-26 21:17:32 +0900

    Add 12-5 Main

--- 12. 브루트 포스/5_1436_영화감독 숌/Main.java
+++ 12. 브루트 포스/5_1436_영화감독 숌/Main.java
@@ -2,6 +2,20 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-	
+		int n = 0; // 입력받은 수
+		int cnt = 1; // 영화제목의 숫자
+		int num = 666;
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		while (cnt != n) {
+			num++;
+			// 문자열에 666이 있는지 검사
+			if (String.valueOf(num).contains("666")) {
+				cnt++;
+			}
+		}
+		System.out.println(num);
+
 	}
+
 }
